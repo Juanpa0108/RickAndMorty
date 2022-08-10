1fffbb7d2a05658a4fd17141660fbdf32fd1572a
Juanpa0108 <109108926+Juanpa0108@users.noreply.github.com>
2022-08-09 22:41:38 -0500

    Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'listaDePersonajes', loadChildren: () => import('./components/pages/personajes/lista-de-personajes/lista-de-personajes.module').then(m => m.ListaDePersonajesModule) }];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'listaDePersonajes', loadChildren: () => import('./components/pages/personajes/lista-de-personajes/lista-de-personajes.module').then(m => m.ListaDePersonajesModule) },
+  { path: 'detallesPersonaje/:id', loadChildren: () => import('./components/pages/personajes/detalles-personaje/detalles-personaje.module').then(m => m.DetallesPersonajeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
